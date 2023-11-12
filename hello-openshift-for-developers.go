010088941ee8f498b2717ec53fa7556c8f5433d9
agadir001 <110976654+agadir001@users.noreply.github.com>
2023-11-12 17:37:12 +0100

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World From OpenShift!DEMO"
+		response = "Hello World From OpenShift!"
 	}
 
 	fmt.Fprintln(w, response)
